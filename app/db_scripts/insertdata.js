37aea540fb8d6c270ca8f1245bec9186e6f26c26
Harrison Stall <hstall2529@gmail.com>
2017-03-17 19:15:21 -0500

    Added campsite names

--- app/db_scripts/insertdata.js
+++ app/db_scripts/insertdata.js
@@ -53,27 +53,27 @@ function loadTestData(callback) {
     // db.collection('users').insert(testUser2);
     // db.collection('users').insert(testUser3);
     
-    var testCampsite = new Campsite({creator: "harrisonstall", rating: 5, description: "There are some cool waterfalls. Highly recommend.",
+    var testCampsite = new Campsite({name: "SomeName", creator: "harrisonstall", rating: 5, description: "There are some cool waterfalls. Highly recommend.",
                                     directions: "Hop the boulder", price: 0, lat: 36.142980, long: -86.805682, size: "small", tags: ["waterfall", "fun"],
                                     fire: true, reviews: [reviewID]});
 
-    var testCampsite2 = new Campsite({creator: "turnerstrayhorn", rating: 5, description: "Great hiking trails! And great hot chicken!",
+    var testCampsite2 = new Campsite({name: "SomeName1", creator: "turnerstrayhorn", rating: 5, description: "Great hiking trails! And great hot chicken!",
                                     directions: "Call ahead to skip the line", price: 15, lat: 36.1514, long: -86.7966, size: "medium", tags: ["wheelchair-accessible", "bathrooms"],
                                     fire: true, reviews: []});
 
-    var testCampsite3 = new Campsite({creator: "turnerstrayhorn", rating: 4, description: "Good southern camping!",
+    var testCampsite3 = new Campsite({name: "SomeName2", creator: "turnerstrayhorn", rating: 4, description: "Good southern camping!",
                                     directions: "Family style", price: 20, lat: 36.1151, long: -86.6901, size: "large", tags: ["wheelchair-accessible", "bathrooms"],
                                     fire: false, reviews: []});
 
-    var testCampsite4 = new Campsite({creator: "turnerstrayhorn", name: "somesite", rating: 4, description: "Testsite",
+    var testCampsite4 = new Campsite({name: "SomeName3", creator: "turnerstrayhorn", name: "somesite", rating: 4, description: "Testsite",
                                     directions: "tbd", price: 20, lat: 36.1351, long: -86.7901, size: "large", tags: ["pet-friendly", "family-friendly"],
                                     fire: false, reviews: []});
 
-    var testCampsite5 = new Campsite({creator: "harrisonstall", name: "someothersite", rating: 4, description: "Testsite",
+    var testCampsite5 = new Campsite({name: "SomeName3", creator: "harrisonstall", name: "someothersite", rating: 4, description: "Testsite",
                                     directions: "tbd", price: 20, lat: 36.2151, long: -86.4901, size: "large", tags: ["family-friendly", "wheelchair-accessible"],
                                     fire: false, reviews: []});
 
-    var testCampsite6 = new Campsite({creator: "longadams", name: "somenewsite", rating: 4, description: "Testsite",
+    var testCampsite6 = new Campsite({name: "SomeName4", creator: "longadams", name: "somenewsite", rating: 4, description: "Testsite",
                                     directions: "tbd", price: 20, lat: 36.1451, long: -86.4901, size: "large", tags: ["pet-friendly", "grill"],
                                     fire: false, reviews: []});
 
