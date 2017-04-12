394ca5a974a61748e66226db1c04ae0fb8ffd010
Harrison Stall <hstall2529@gmail.com>
2017-04-11 21:17:43 -0500

    Slash again

--- app/server/server.js
+++ app/server/server.js
@@ -94,7 +94,7 @@ let transporter = nodemailer.createTransport({
     }
 });
 
-var folder = process.env.PWD + '/public';
+var folder = (process.env.PWD + '/public').slice(1);
 console.log(folder)
 app.use(express.static(/*path.join(process.env.PWD, '../public/')*/folder));
 var sess = {
